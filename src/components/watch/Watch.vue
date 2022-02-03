e123c2b2f97e9f37eed9652a0657f311b2ca27fe
조호진 <hojin99@naver.com>
2022-02-03 22:36:49 +0900

    watch, computed

--- src/components/watch/Watch.vue
+++ src/components/watch/Watch.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>...</div>
+  <div>
+    <el-input placeholder="Please Input Text" v-model="message"></el-input>
+    <p>method : {{getMessage()}} </p> 
+    <p>method : {{getMessage()}} </p> 
+    <p>computed : {{reverseMessage}} </p>
+    <!-- 두번째는 호출 안됨 (캐싱) -->
+    <p>computed : {{reverseMessage}} </p> 
+    <p>watch : {{rMessage}} </p> 
+    <p>watch : {{rMessage}} </p> 
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      message:'test',
+      rMessage:'tset'
+    }
+  }, 
+  methods: {
+    getMessage() {
+      console.log('getMessage');
+      return this.message.split('').reverse().join('');
+    }
+  }, 
+  computed: {
+    reverseMessage: function() {
+      console.log('reverseMessage');
+      return this.message.split('').reverse().join('');
+    }
+  },
+  watch: {
+    message : function (val) {
+      console.log('watch');
+      this.rMessage = val.split('').reverse().join('');
+    }
+  }
 }
 </script>
 
-<style>
+
+<style scoped>
+
+.el-input {
+  width: 200px;
+}
 
 </style>
